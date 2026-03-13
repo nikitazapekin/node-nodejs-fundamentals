f4ffe87aa3e9846ecf19570b6637b1b4d7bf84c7
Nikita <nikitazapekin>
2026-03-13 21:11:32 +0300

    feat(init): Initialize branch

--- src/modules/plugins/repeat.js
+++
@@ -1,3 +0,0 @@
-export function run() {
-  return 'repeat repeat repeat';
-}
--- src/wt/worker.js
+++
@@ -1,9 +0,0 @@
-import { parentPort } from 'worker_threads';
-
-if (parentPort) {
-  parentPort.on('message', (numbers) => {
-  
-    const sorted = numbers.sort((a, b) => a - b);
-    parentPort.postMessage(sorted);
-  });
-}
