9712ef61377d9c968cf33300359df160a6c142ba
Nikita <nikitazapekin>
2026-03-13 21:22:02 +0300

    feat(branch): Init branch

--- src/hash/verify.js
+++
@@ -1,8 +0,0 @@
-const verify = async () => {
-  // Write your code here
-  // Read checksums.json
-  // Calculate SHA256 hash using Streams API
-  // Print result: filename — OK/FAIL
-};
-
-await verify();
--- src/wt/worker.js
+++
@@ -1,9 +0,0 @@
-import { parentPort } from 'worker_threads';
-
-// Receive array from main thread
-// Sort in ascending order
-// Send back to main thread
-
-parentPort.on('message', (data) => {
-  // Write your code here
-});
